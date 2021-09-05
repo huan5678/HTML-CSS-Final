98eada2603e486b5f6426a2887841e0e410030b5
huan5678 <huan5678@gmail.com>
2021-09-05 23:19:30 +0800

    初步完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,39 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+const navList = $('#nav-list');
+
+const header = $('header');
+let lastScroll = 0;
+
+$("#nav-btn").on('click',()=>{
+  // console.log('click button')
+  if(navList.hasClass('d-none')){
+    navList.removeClass('d-none')
+  } else {
+    navList.addClass('d-none')
+  }
+})
+
+window.addEventListener('scroll',function(){
+  const currentScroll = window.pageYOffset;
+
+  if(currentScroll <= 0){
+    header.removeClass('scroll--up');
+    return;
+  }
+
+  if(currentScroll > lastScroll && !header.hasClass('scroll--down')){
+    header.removeClass('scroll--up');
+    header.addClass('scroll--down');
+  } else if(currentScroll < lastScroll && header.hasClass('scroll--down')){
+    header.removeClass('scroll--down');
+    header.addClass('scroll--up');
+  }
+  lastScroll = currentScroll;
+})
+
+const mainHeight = $('main')
+
+// console.log(mainHeight.height())
+$(window).on('resize',()=>{
+  $('footer').css('margin-top',(mainHeight.height()-header.height()))
+})
+$('footer').css('margin-top',(mainHeight.height()-header.height()))
--- gulpfile.js/envOptions.js
+++ gulpfile.js/envOptions.js
@@ -32,12 +32,10 @@ let envOptions = {
     src: [
       `${srcPath}/assets/style/**/*.scss`,
       `${srcPath}/assets/style/**/*.sass`,
-      `!${srcPath}/assets/style/**/_variables.scss`,
-      `!${srcPath}/assets/style/**/bootstrap.scss`,
     ],
-    bsSrc: [
-      `${srcPath}/assets/style/helpers/bootstrap.scss`,
-    ],
+    // bsSrc: [
+    //   `${srcPath}/assets/style/helpers/bootstrap.scss`,
+    // ],
     outputStyle: 'expanded',
     path: `${distPath}/assets/style`,
   },
@@ -51,7 +49,15 @@ let envOptions = {
   vendors: {
     src: [
       `${nodePath}/jquery/dist/jquery.slim.min.js`,
-      `${nodePath}/bootstrap/dist/js/bootstrap.bundle.min.js`, // 已包含 popper.js
+      // `${nodePath}/bootstrap/dist/js/bootstrap.bundle.min.js`, // 已包含 popper.js
+      // `${nodePath}/aos/dist/aos.js`,
+      // `${nodePath}/swiper/swiper-bundle.js`,
+      // `${nodePath}/lax.js/lib/lax.min.js`,
+      // `${nodePath}/@fortawesome/fontawesome-free/js/brands.js`,
+      // `${nodePath}/@fortawesome/fontawesome-free/js/solid.js`,
+      // `${nodePath}/@fortawesome/fontawesome-free/js/fontawesome.js`,
+      // `${nodePath}/vanillajs-datepicker/dist/js/datepicker-full.js`,
+      // `${nodePath}/jquery-mousewheel/jquery.mousewheel.js`,
     ],
     concat: 'vendors.js',
     path: `${distPath}/assets/js`,
