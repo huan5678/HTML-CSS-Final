5189a944cbdf74408bb8c71640fe363d1f790e78
huan5678 <huan5678@gmail.com>
2021-09-21 12:51:12 +0800

    fixed

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,39 +1,45 @@
-const navList = $('#nav-list');
+const navList = $("#nav-list");
 
-const header = $('header');
+const header = $("header");
 let lastScroll = 0;
 
-$("#nav-btn").on('click',()=>{
+$("#nav-btn").on("click", () => {
   // console.log('click button')
-  if(navList.hasClass('d-none')){
-    navList.removeClass('d-none')
+  if (navList.hasClass("d-none")) {
+    navList.removeClass("d-none");
   } else {
-    navList.addClass('d-none')
+    navList.addClass("d-none");
   }
-})
+});
 
-window.addEventListener('scroll',function(){
+window.addEventListener("scroll", function () {
   const currentScroll = window.pageYOffset;
 
-  if(currentScroll <= 0){
-    header.removeClass('scroll--up');
+  if (currentScroll <= 0) {
+    header.removeClass("scroll--up");
     return;
   }
 
-  if(currentScroll > lastScroll && !header.hasClass('scroll--down')){
-    header.removeClass('scroll--up');
-    header.addClass('scroll--down');
-  } else if(currentScroll < lastScroll && header.hasClass('scroll--down')){
-    header.removeClass('scroll--down');
-    header.addClass('scroll--up');
+  if (currentScroll > lastScroll && !header.hasClass("scroll--down")) {
+    header.removeClass("scroll--up");
+    header.addClass("scroll--down");
+  } else if (currentScroll < lastScroll && header.hasClass("scroll--down")) {
+    header.removeClass("scroll--down");
+    header.addClass("scroll--up");
   }
   lastScroll = currentScroll;
-})
+});
 
-const mainHeight = $('main')
+// const mainHeight = $("main");
 
-// console.log(mainHeight.height())
-$(window).on('resize',()=>{
-  $('footer').css('margin-top',(mainHeight.height()-header.height()))
-})
-$('footer').css('margin-top',(mainHeight.height()-header.height()))
+// // console.log(mainHeight.height())
+// $(window).on("resize", () => {
+//   $("footer").css(
+//     "margin-top",
+//     mainHeight.height() - header.height()
+//   );
+// });
+// $("footer").css(
+//   "margin-top",
+//   mainHeight.height() - header.height()
+// );
